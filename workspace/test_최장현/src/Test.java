ab7d292ed83582e2039934fc7ba9bf131f4e8d5e
jh011228 <jh01122844@gmail.com>
2025-07-09 08:44:34 +0900

    250709 08:44 ㅇㅇ

--- workspace/test_최장현/src/Test.java
+++ workspace/test_최장현/src/Test.java
@@ -5,17 +5,36 @@ public class Test {
 		Scanner sc = new Scanner(System.in); 
 		int count = sc.nextInt();
 		
-		String[] arr = new String[count];
-			arr[0] = sc.next();
+			String[] arr = new String[count];
 			boolean[] alpha = new boolean[26];
 			int countScore = 0;
-			boolean isTrue = true;
-			for(int i=0;i<arr[0].length();i++) {
-				alpha[arr[0].charAt(i)-'a'] = true; 
-			}
-			if(isTrue) {
+			boolean isTrue = false;
+			for(int k=0;k<count;k++) {
+				arr[k] = sc.next();
+				for(int i=0;i<arr[k].length();i++) {
+					if(alpha[arr[k].charAt(i)-'a']==false) {
+						alpha[arr[k].charAt(i)-'a'] = true; 					
+					}
+					//옆자리인지 체크
+					else {
+						for(int j=0;j>i;j++) {
+							int b = arr[k].charAt(i)-'a' - arr[k].charAt(j)-'a';
+							if(b==0) {
+								if(i-j==1||i-j==-1) {
+									isTrue = true;
+								}
+								else {
+									isTrue = false;
+								}
+							}
+						}
+					}
+					
+				}
+				if(isTrue) {
 					countScore++;
 				}
+			}
 			System.out.println(countScore);
 	}
 }
